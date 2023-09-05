d9b4e11aea0b0caa18a5eee9d8bbb38ec7b8d2b0
jareyesore <144158276+jareyesore@users.noreply.github.com>
2023-09-05 12:17:03 -0600

    Add files via upload
    Primer programa de operaciones matemáticas básicas

---
+++ L1-JARO_1067623.py
@@ -0,0 +1,25 @@
+
+#Introducción a la Programación - sección: 17
+#05/09/2023
+#José Andrés Reyes Orellana
+#Objetivo: Crear un programa en Python que realice operaciones aritméticas básicas
+#Entrada: Numero1, Numero2
+"""Proceso: 
+1. Solicitar al usuario dos números enteros y realizar calculos de aritmética básica 
+2. Mostrar los resultados
+"""
+"""Salida:
+1. Resultado de suma
+2. Resultado de resta
+3. Resultado de multipñlicación
+4. Resultado de división
+"""
+print("Mi primer programa")
+N1 = input("Numero1 = ")
+N2 = input("Numero2 = ")
+N1 = int(N1)
+N2 = int(N2)
+print(N1+N2)
+print(N1-N2)
+print(N1*N2)
+print(N1/N2)
