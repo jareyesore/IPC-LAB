647e6863fe51598b2a5e7fc2f32f1a41184c698f
jareyesore <144158276+jareyesore@users.noreply.github.com>
2023-09-07 09:43:58 -0600

    Add files via upload
    Segundo programa

---
+++ TS5-JARO_1067623.py
@@ -0,0 +1,28 @@
+#Introduccion a la Programacion - seccion: 17
+#07/09/2023
+#Jose Andres Reyes Orellana
+#Objetivo: Crear un programa capaz de mostrar el nombre, la edad, la carrera y el carne de un alumno
+#Entrada: nombre, edad, carrera, carne
+"""Proceso: 
+1. Solicitar al usuario su nombre
+2. Solicitar al usuario su edad
+3. Solicitar al usuario su carrera
+4. Solicitar al usuario su carne
+5. Mostrar mensaje final
+"""
+"""Salida:
+1. Se mostrara un mensaje final con toda la informacion recolectada
+"""
+
+print("Mi Segundo Programa")
+sNombre = str(input("Ingrese su nombre = "))
+sEdad = str(input("Ingrese su edad = "))
+sCarrera = str(input("Ingrese su carrera = "))
+sCarne = str(input("Ingrese su numero de carne = "))
+
+print(sNombre)
+print(sEdad)
+print(sCarrera)
+print(sCarne)
+
+print("soy " ,sNombre, " tengo " ,sEdad, " años y estudio la carrera de " ,sCarrera, " mi numero de carne es: " ,sCarne)
